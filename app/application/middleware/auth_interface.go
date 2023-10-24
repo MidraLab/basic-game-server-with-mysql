7409497fafd058896355ec590890f286fb3c15d3
eiei114 <nowaka37564@gmail.com>
2023-10-25 01:35:30 +0900

    go generateに対応させた

--- app/application/middleware/auth_interface.go
+++ app/application/middleware/auth_interface.go
@@ -2,6 +2,7 @@ package middleware
 
 import "github.com/uptrace/bunrouter"
 
+//go:generate mockgen -source=auth_interface.go -destination=../../mocks/auth_mocks.go -package=mocks
 type MiddlewareInterface interface {
 	AuthenticateMiddleware() func(bunrouter.HandlerFunc) bunrouter.HandlerFunc
 	CorsMiddleware() func(bunrouter.HandlerFunc) bunrouter.HandlerFunc
