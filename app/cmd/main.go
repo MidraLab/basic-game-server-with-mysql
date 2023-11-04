b58f6304d7d08e613b61b030b02b1cd9228139ac
eiei114 <nowaka37564@gmail.com>
2023-11-05 04:28:43 +0900

    fix routerファイルの追加

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -13,8 +13,8 @@ import (
 	"example.com/application/service"
 	"example.com/config"
 	infrastructure "example.com/infrastructure/persistence"
-	_interface "example.com/interface/handler"
-	"github.com/uptrace/bunrouter"
+	handler "example.com/interface/handler"
+	router "example.com/interface/router"
 	"log"
 	"net/http"
 )
@@ -25,21 +25,10 @@ func main() {
 
 	userRepository := infrastructure.NewUserRepository(db)
 	userService := service.NewUserService(userRepository)
-	userHandler := _interface.NewUserHandler(userService)
+	userHandler := handler.NewUserHandler(userService)
 	middleware := middleware.NewMiddleware(userService)
-
-	r := bunrouter.New()
-	r.Use(middleware.RecoverMiddleware())
-	r.Use(middleware.CorsMiddleware())
-
-	r.POST("/user/create", userHandler.UserCreateHandle())
-
-	r.Use(middleware.AuthenticateMiddleware()).WithGroup("", func(group *bunrouter.Group) {
-		group.POST("/user/get", userHandler.UserGetHandle())
-		group.POST("/user/score", userHandler.ScoreUpdateHandle())
-		group.POST("/user/destroy", userHandler.DestroyHandle())
-		group.GET("/users/get", userHandler.UserRankingGetHandle())
-	})
+	router := router.NewRouter(userHandler, middleware)
+	r := router.InitRouter()
 
 	log.Println("listening on http://localhost:8080")
 	log.Println(http.ListenAndServe(":8080", r))
